a5670a5f304c8a4db62d126d2dee98abebd90081
NhatTran-97 <nhat.tranduy1997@gmail.com>
2025-05-23 00:37:23 +0700

    Commit of nhatbot_stack

--- nhatbot_stack/launch/nhatbot_bringup.launch.py
+++ nhatbot_stack/launch/nhatbot_bringup.launch.py
@@ -10,6 +10,10 @@ from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
 
+    use_sim_time_arg = DeclareLaunchArgument(name="use_sim_time", default_value="False",
+                                      description="Use simulated time"
+    )
+
     # Declare launch arguments
     wheel_radius_arg = DeclareLaunchArgument("wheel_radius", default_value="0.0535")
     wheel_separation_arg = DeclareLaunchArgument("wheel_separation", default_value="0.45")
@@ -30,27 +34,26 @@ def generate_launch_description():
         package="joy",
         executable="joy_node",
         name="joystick",
-        parameters=[os.path.join(nhatbot_stack_pkg, "config", "joy_config.yaml")]
-    )
+        parameters=[os.path.join(nhatbot_stack_pkg, "config", "joy_config.yaml")])
 
-    # Joy Teleop node
-    joy_teleop = Node(
-        package="joy_teleop",
-        executable="joy_teleop",
-        parameters=[os.path.join(nhatbot_stack_pkg, "config", "joy_teleop.yaml")]
-    )
+
+    twist_relay_node = Node(
+        package="nhatbot_twist_teleop",
+        executable="twist_relay",
+        name="twist_relay",
+        parameters=[{"use_sim_time": LaunchConfiguration("use_sim_time")}])
     
     # Twist Teleop node
     joy_to_twist = Node(
-        package="twist_teleop",
+        package="nhatbot_twist_teleop",
         executable="joy_to_twist",
         name="joy_to_twist",
-        # output="screen",
+        output="screen",
         parameters=[
-            {"linear_scale": 0.5},          # ✅ Hệ số tốc độ tiến/lùi
-            {"angular_scale": 0.5},         # ✅ Hệ số tốc độ quay
-            {"deadman_button": 9},          # ✅ Nút giữ để di chuyển
-            {"deadzone_threshold": 0.1}     # ✅ Ngưỡng tối thiểu tránh trôi
+            {"linear_scale": 0.5},        
+            {"angular_scale": 0.5},        
+            {"deadman_button": 9},          
+            {"deadzone_threshold": 0.01}  
         ]
 
     )
@@ -61,8 +64,11 @@ def generate_launch_description():
             os.path.join(twist_mux_pkg, "launch", "twist_mux_launch.py")
         ),
         launch_arguments={
-            "cmd_vel_out": "nhatbot_controller/cmd_vel",
+            "cmd_vel_out": "/nhatbot/cmd_vel_unstamped",
             "config_topics": os.path.join(nhatbot_stack_pkg, "config", "twist_mux_topics.yaml"),
+            "config_locks": os.path.join(nhatbot_stack_pkg, "config", "twist_mux_locks.yaml"),
+            "config_joy":  os.path.join(nhatbot_stack_pkg, "config", "twist_mux_joy.yaml"),
+            "use_sim_time":  LaunchConfiguration("use_sim_time")
         }.items()
     )
 
@@ -79,7 +85,7 @@ def generate_launch_description():
         package="differential_drive",
         executable="differential_drive_controller.py",
         arguments=[{"wheel_radius": wheel_radius,
-                    "wheel_separation": wheel_separation}],
+                    "wheel_separation": wheel_separation,}],
         condition=IfCondition(use_simple_controller)
     )
 
@@ -90,18 +96,20 @@ def generate_launch_description():
             name="motor_controller",
             output="screen",
             parameters=[{
-                "wheel_radius": 0.0535,
-                "wheel_separation": 0.45
+                "wheel_radius": wheel_radius,
+                "wheel_separation": wheel_separation,
+                "linear_velocity_max_": 0.5,
+                "angular_velocity_max_": 0.5
             }]
         )
     odom_estimator_node = Node(
             package="differential_drive",
-            executable="odom_estimator_node",
+            executable="odom_estimator_node.py",
             name="odom_estimator",
             output="screen",
             emulate_tty=True,
             parameters=[{
-                "wheel_radius": 0.0535, "wheel_separation": 0.45}])
+                "wheel_radius":wheel_radius, "wheel_separation": wheel_separation, "enable_tf_broadcast:": True}])
 
     # Include rosbridge server launch
     rosbridge_launch = IncludeLaunchDescription(
@@ -114,7 +122,7 @@ def generate_launch_description():
     reset_feedback_service = ExecuteProcess(
         cmd=[
             "ros2", "service", "call",
-            "/nhatbot_firmware/reset_feedback_position",
+            "/nhatbot/reset_feedback_position",
             "std_srvs/srv/Trigger"
         ],
         output="screen"
@@ -130,16 +138,9 @@ def generate_launch_description():
         output="screen"
     )
 
-        # Ensure reset_odom_service runs only after reset_feedback_service completes
-    # reset_odom_handler = RegisterEventHandler(
-    #     OnProcessExit(
-    #         target_action=reset_feedback_service,
-    #         on_exit=[reset_odom_service]
-    #     )
-    # )
 
     lidar_node = IncludeLaunchDescription(
-        os.path.join(get_package_share_directory("rplidar_ros"),"launch", "rplidar_a1_launch.py"),)
+        os.path.join(get_package_share_directory("nhatbot_stack"),"launch", "lidar_a1_filter.launch.py"),)
     
 
     static_pub =  Node(
@@ -149,16 +150,18 @@ def generate_launch_description():
             
     # Return the LaunchDescription
     return LaunchDescription([
+        use_sim_time_arg,
         wheel_radius_arg,
         wheel_separation_arg,
         use_simple_controller_arg,
         joy_node,
+        twist_relay_node,
         joy_to_twist,
-        # twist_mux_launch,
+        twist_mux_launch,
         zlac_driver,
         # differential_drive_controller,
         motor_controller_node,
-        odom_estimator_node,
+        # odom_estimator_node,
         rosbridge_launch,
         reset_feedback_service,
         lidar_node,
