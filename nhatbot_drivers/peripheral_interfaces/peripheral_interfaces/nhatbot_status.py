81c818ab6db68704e0cb6023d1aa2aaf0b57c194
NhatTran-97 <nhat.tranduy1997@gmail.com>
2025-08-10 00:23:26 +0700

    Add peripheral_interfaces package

---
+++ nhatbot_drivers/peripheral_interfaces/peripheral_interfaces/nhatbot_status.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from dataclasses import dataclass
+import Jetson.GPIO as GPIO
+import time
+
+@dataclass(frozen=True)
+class Pins:
+    robot_ready: int = 23
+    robot_warning: int = 15
+    robot_error: int = 35
+    status_buzzer: int = 13  # ⚠️ Nếu dùng PWM thì nên đổi sang Pin 32
+    full_motor_battery: int = 31
+    mid_motor_battery: int = 12
+    low_motor_battery: int = 29
+    full_computer_battery: int = 33
+    mid_computer_battery: int = 7
+    low_computer_battery: int = 21
+
+class NhatbotStatus(Node):
+    def __init__(self):
+        super().__init__('nhatbot_status')
+
+        self.pins = Pins()
+        self.led_state = False     # ✅ Trạng thái LED
+        self.blink = False         # ✅ Trạng thái chớp tắt LED
+
+        self.setup_gpio()
+        self.timer = self.create_timer(0.1, self.timer_callback)  # Nhấp nháy 10Hz
+
+    def setup_gpio(self):
+        GPIO.setmode(GPIO.BOARD)
+        for name, pin in vars(self.pins).items():
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, GPIO.LOW)  # Khởi tạo tất cả pin ở trạng thái LOW
+
+        self.active_robot_ready(True)  # Bật LED khi khởi động
+
+    def active_robot_ready(self, on: bool):
+        if self.led_state != on:
+            GPIO.output(self.pins.robot_ready, GPIO.HIGH if on else GPIO.LOW)
+            self.led_state = on
+
+    def timer_callback(self):
+        self.blink = not self.blink
+        # self.active_robot_ready(self.blink)
+
+    def beep_pattern(self, on_time=0.2, off_time=0.3, repeat=3):
+        """
+        Bật/tắt còi theo tần suất lặp lại.
+
+        Args:
+            on_time (float): thời gian bật (giây)
+            off_time (float): thời gian tắt (giây)
+            repeat (int): số lần kêu
+        """
+        for _ in range(repeat):
+            GPIO.output(self.pins.status_buzzer, GPIO.HIGH)
+            time.sleep(on_time)
+            GPIO.output(self.pins.status_buzzer, GPIO.LOW)
+            time.sleep(off_time)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = NhatbotStatus()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        GPIO.cleanup()  # Quan trọng để reset trạng thái chân GPIO
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
